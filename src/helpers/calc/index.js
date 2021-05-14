2ca11e1aaed2eee45e9dbfa49af00c42accb9b11
Railson Rodrigues <rrrailson1@gmail.com>
2021-05-14 19:02:22 -0400

    refactor: add percentage calc

--- src/helpers/calc/index.js
+++ src/helpers/calc/index.js
@@ -2,25 +2,42 @@ import discountINSS from './discountINSS.js';
 import discountIRRF from './discountIRRF.js';
 import discountDependents from './discountDependents.js';
 
-export default function calcSalary(grossSalary = 0, nDependents = 0, otherDiscounts = 0) {
-  const inss = discountINSS(grossSalary);
-  const dependentsCost = discountDependents(nDependents);
-  const irrf = discountIRRF(grossSalary - inss - dependentsCost);
+function sum(...summation) {
+  return summation.reduce((acc, currentValue) => acc + currentValue, 0);
+}
 
-  const totalDiscount = inss + irrf + otherDiscounts;
-  const netSalary = grossSalary - totalDiscount;
+function subtraction(amount, ...subtrations) {
+  let result = amount;
+
+  subtrations.forEach(value => {
+    result -= value;
+  });
+
+  return result;
+}
+
+function percentageOf(totalValue, fractionValue) {
+  return fractionValue * 100 / totalValue;
+}
+
+export default function calcSalary(grossSalary = 0, nDependents = 0, otherDiscounts = 0) {
+  const dependentsCost = discountDependents(nDependents);
+  
+  const inss = discountINSS(grossSalary);
+  const irrf = discountIRRF(subtraction(grossSalary, inss, dependentsCost));
+  const totalDiscount = sum(inss, irrf, otherDiscounts);
+  const netSalary = subtraction(grossSalary, totalDiscount);
+
+  const inssPercent = percentageOf(grossSalary, inss);
+  const irrfPercent = percentageOf(grossSalary, irrf);
+  const otherDiscountsPercent = percentageOf(grossSalary, otherDiscounts);
+  const totalDiscountPercent = percentageOf(grossSalary, totalDiscount);
+  const netSalaryPercent = percentageOf(grossSalary, netSalary);
 
   return {
-    discountINSS: inss,
-    discountIRRF: irrf,
-    otherDiscounts: otherDiscounts,
-    totalDiscount,
-    netSalary,
-    // netSalary: grossSalary - this.totalDiscount,
-    // discountINSSpercent: ,
-    // discountIRRFpercent: ,
-    // otherDiscountspercent: ,
-    // totalDiscountPercent: ,
-    // netSalaryPercent: ,
+    inss, irrf, totalDiscount, netSalary,
+    inssPercent, irrfPercent, otherDiscountsPercent, totalDiscountPercent, netSalaryPercent
   }
 }
+
+console.log(calcSalary(22500, 0, 85000));
