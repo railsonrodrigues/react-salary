d0f1298baa71470ab3c18557cce32df794a58135
Railson Rodrigues <rrrailson1@gmail.com>
2021-05-15 11:34:10 -0400

    finished: add dynamic state

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ import FlatCard from "../../components/FlatCard/FlatCard";
 import Graphic from "../../components/Graphic/Graphic";
 import Form from "../../components/Form/Form";
 
-import calcSalary from './../../helpers/calc';
+import getResultCalc from './../../helpers/calc/interface/getResult.js';
 
 export default function App() {
 
@@ -14,7 +14,7 @@ export default function App() {
   const [totalDiscount, setTotalDiscount] = useState(0);
   const [inss, setInss] = useState(0);
   const [irrf, setIrrf] = useState(0);
-  const [otherDiscounts] = useState(0);
+  const [otherDiscounts, setOtherDiscounts] = useState(0);
 
   const [netSalaryPercent, setNetSalaryPercent] = useState(0);
   const [totalDiscountPercent, setTotalDiscountPercent] = useState(0);
@@ -31,7 +31,20 @@ export default function App() {
     const inputnDependents = Number(inputs.find(input => input.id === "nDependents").value);
     const inputOtherDiscounts = Number(inputs.find(input => input.id === "otherDiscounts").value);
 
-    console.log(calcSalary(inputGrossSalary, inputnDependents, inputOtherDiscounts));
+    const result = getResultCalc(inputGrossSalary, inputnDependents, inputOtherDiscounts);
+
+    setNetSalary(() => result.netSalary);
+    setTotalDiscount(() => result.totalDiscount);
+    setInss(() => result.inss);
+    setIrrf(() => result.irrf);
+    setOtherDiscounts(() => result.otherDiscounts);
+
+
+    setNetSalaryPercent(() => result.netSalaryPercent);
+    setTotalDiscountPercent(() => result.totalDiscountPercent);
+    setInssPercent(() => result.inssPercent);
+    setIrrfPercent(() => result.irrfPercent);
+    setOtherDiscountsPercent(() => result.otherDiscountsPercent);
   }
 
   return (
@@ -52,106 +65,3 @@ export default function App() {
     </>
   );
 }
-
-// export default class App extends Component {
-
-//   handleForm = (event) => {
-//     // criar um componente para form e refatorar essa função
-//     event.preventDefault();
-
-//     const salaryValue = event.target[0].value;
-//     const {
-//       inssFormatted,
-//       irrfFormatted,
-//       salLiquidoFormatted,
-//       salLiquidoPercent,
-//       discountPercentINSS,
-//       discountPercentIRRF,
-//     } = calcSalary(salaryValue);
-
-//     this.setState({
-//       currentSalary: salaryValue,
-//       baseINSS: inssFormatted.baseValue,
-//       discountINSS: inssFormatted.discount,
-//       percentDiscountINSS: discountPercentINSS,
-//       baseIRRF: irrfFormatted.baseValue,
-//       discountIRRF: irrfFormatted.discount,
-//       percentDiscountIRRF: discountPercentIRRF,
-//       liquidSalary: salLiquidoFormatted,
-//       liquidSalaryPercent: salLiquidoPercent,
-//     });
-//   };
-
-//   render() {
-//     const {
-//       baseINSS,
-//       baseIRRF,
-//       discountINSS,
-//       discountIRRF,
-//       liquidSalary,
-//       percentDiscountINSS,
-//       percentDiscountIRRF,
-//       liquidSalaryPercent,
-//     } = this.state;
-
-//     return (
-//       <div className={css.appContainer}>
-//         <Header
-//           title="React Salary"
-//           description="Calcule aqui o seu salário líquido"
-//         />
-//         <form onSubmit={this.handleForm}>
-//           <Input
-//             id={css.grossSalary}
-//             type="number"
-//             description="Seu salário (R$)"
-//             readOnly={false}
-//             step="0.01"
-//           />
-//           <Input
-//             id={css.baseInss}
-//             type="text"
-//             description="Base INSS"
-//             readOnly={true}
-//             value={baseINSS}
-//           />
-//           <Input
-//             id={css.descInss}
-//             type="text"
-//             description="Desconto INSS"
-//             readOnly={true}
-//             value={`${discountINSS} (${percentDiscountINSS}%)`}
-//           />
-//           <Input
-//             id={css.baseIrrf}
-//             type="text"
-//             description="Base IRRF"
-//             readOnly={true}
-//             value={baseIRRF}
-//           />
-//           <Input
-//             id={css.descIrrf}
-//             type="text"
-//             description="Desconto IRRF"
-//             readOnly={true}
-//             value={`${discountIRRF} (${percentDiscountIRRF}%)`}
-//           />
-//           <Input
-//             id={css.liquidSalary}
-//             type="text"
-//             description="Salário Líquido"
-//             readOnly={true}
-//             value={`${liquidSalary} (${liquidSalaryPercent}%)`}
-//           />
-//           <SubmitButton id={css.submitButton} value="Calcular" />
-//         </form>
-//         <Graphic
-//           one={liquidSalaryPercent}
-//           two={percentDiscountINSS}
-//           three={percentDiscountIRRF}
-//         />
-//         <hr />
-//       </div>
-//     );
-//   }
-// }
