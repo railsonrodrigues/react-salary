ad47ef25710fff235bb26d5008ebcc8730fa5348
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-03 10:55:04 -0400

    add button styled components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,22 @@
 import React from "react";
-import './styles.css';
+import styled from 'styled-components';
+
+const ButtonStyle = styled.button`
+  border: 2px solid #ce19e6;
+  background: #fff;
+  color: #ce19e6;
+  font-weight: 700;
+  height: 50px;
+  width: 100%;
+  border-radius: 5px;
+  transition-duration: 200ms;
+  &:hover {
+    cursor: pointer;
+    background: #ce19e6;
+    color: #fff;
+  }
+`;
 
 export default function Button({ value }) {
-  return <button>{value}</button>;
+  return <ButtonStyle>{value}</ButtonStyle>;
 }
