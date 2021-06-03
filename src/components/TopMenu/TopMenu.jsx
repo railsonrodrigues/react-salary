fe5a0efdfaa90d3bd5f39b050b85208050755772
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-03 11:15:25 -0400

    add to top menu styled components

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -2,36 +2,60 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const NavWrapper = styled.nav`
+  margin-bottom: 50px;
+`;
+
+const Logo = styled.h1`
+  position: absolute;
+  transform: translateY(-50%);
+  top: 50%;
+  left: 25px;
+  font-size: 18px;
+  font-weight: 400;
+  font-family: sans-serif;
+  color: #fff;
+`;
+
+const Purple = styled.span`
+  color: #ce19e6;
+`;
+
 const NavBar = styled.ul`
+  position: relative;
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
+  gap: 60px;
   width: 100vw;
-  height: 50px;
-  border: 1px solid red;
+  height: 100px;
+  background: #1c1c1c;
 `
 
 const Menu = styled.li`
   display: inline-block;
-  border: 1px solid red;
   width: 100%;
   height: 100%;
+  color: #fff;
+  opacity: 0.4;
+  transition-duration: 200ms;
   &:hover {
-    background: red;
+    opacity: 1;
   }
 `
 
 export default function TopMenu ({ pathname }) {
   if (pathname === '/home' || pathname === '/account' || pathname === '/about-me') {
     return (
-      <nav>
+      <NavWrapper>
         <NavBar>
+          <Logo>Railson<Purple>Calc</Purple></Logo>
           <Link to="/home"><Menu>Início</Menu></Link>
           <Link to="/about-me"><Menu>Sobre mim</Menu></Link>
           <Link to="/account"><Menu>Conta</Menu></Link>
           <Link to="/"><Menu>Logout</Menu></Link>
         </NavBar>
-      </nav>
+      </NavWrapper>
     )
   }
 
