4bdbb43d7db58b7b8c97a41957516235bbd24f0b
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-03 11:03:28 -0400

    add input styled components

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,23 @@
 import React from "react";
-import "./styles.css";
+import styled from 'styled-components';
+
+const InputStyle = styled.input`
+  height: 50px;
+  width: 100%;
+  border: none;
+  border-bottom: 2px solid #aaa;
+  padding: 0 10px;
+  transition-duration: 200ms;
+  &::placeholder {
+    color: #818181;
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-bottom: 2px solid #ce19e6;
+  }
+`;
 
 export default function Input({ placeholder, id }) {
-  return <input id={id} type="text" placeholder={placeholder} />;
+  return <InputStyle id={id} type="text" placeholder={placeholder} />;
 }
