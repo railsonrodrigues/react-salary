6b53768d2d12b85d5001dd0f38f25e420320dae0
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-03 10:05:03 -0400

    add styled components to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import './styles.css';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px 0;
+  width: 200px;
+  height: 150px;
+  border-radius: 5px;
+  box-shadow: rgba(206, 25, 230, 0.2) 0px 8px 24px;
+`;
+
+const Title = styled.h3`
+  font-size: 14px;
+  font-weight: 400;
+  color: #818181;
+`;
+
+const Amount = styled.span`
+  font-size: 32px;
+  font-weight: 700;
+  color: #ce19e6;
+`;
+
+const Percentage = styled.span`
+  font-size: 20px;
+  font-weight: 400;
+  color: #818181;
+`;
 
 export default function Card ({ title, value, percentage }) {
   return (
-    <div className="card">
-      <strong>{ title }</strong>
-      <span>{ value }</span>
-      <span>{ percentage }</span>
-    </div>
+    <CardWrapper>
+      <Title>{ title }</Title>
+      <Amount>{ value }</Amount>
+      <Percentage>{ percentage }</Percentage>
+    </CardWrapper>
   )
 }
