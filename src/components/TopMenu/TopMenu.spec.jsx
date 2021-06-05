fa782834f43f197970183153a63e07ce5a6d871b
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-05 11:41:36 -0400

    add TopMenu tests

---
+++ src/components/TopMenu/TopMenu.spec.jsx
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import TopMenu from './TopMenu';
+
+describe('<TopMenu />', () => {
+  it('should render TopMenu correctly', () => {
+    const { container } = render(
+      <Router>
+        <TopMenu />
+      </Router>
+    )
+
+    const topMenu = screen.getByRole('navigation')
+    expect(topMenu).toBeInTheDocument();
+  })
+})
