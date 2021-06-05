037689f07d170a1a7fa1a7938a655c78a323507e
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-04 20:14:03 -0400

    add flatCard tests

---
+++ src/components/FlatCard/FlatCard.spec.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import React from 'react';
+import FlatCard from './FlatCard';
+
+describe('<FlatCard />', () => {
+  it('should render FlatCard correctly', () => {
+    const {container} = render(<FlatCard title="INSS" value="700" percentage="30" />)
+    const flatCard = container.querySelector('div');
+    const titleElement = container.querySelector('h3.sc-dlnjwi');
+    const amountElement = container.querySelector('span.sc-gtsrHT');
+    const percentageElement = container.querySelector('span.kquQli');
+
+    expect(flatCard).toBeInTheDocument();
+    expect(titleElement).toBeInTheDocument();
+    expect(amountElement).toBeInTheDocument();
+    expect(percentageElement).toBeInTheDocument();
+  })
+})
