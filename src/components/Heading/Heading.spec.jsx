f7c8d4443ea911924c0e83bee124c0a94cfc1f17
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-04 20:13:42 -0400

    add heading tests

---
+++ src/components/Heading/Heading.spec.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import React from 'react';
+import Heading from './Heading';
+
+describe('<Heading />', () => {
+  it('should render heading with text', () => {
+    render(<Heading title="Hello World!" align="center" />)
+    const heading = screen.getByText('Hello World!');
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent('Hello World!');
+  })
+})
