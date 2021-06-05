9c8099821007a42cc4f8f64f36b8f7c8dc5b62e4
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-04 20:01:05 -0400

    add card tests

---
+++ src/components/Card/Card.spec.jsx
@@ -0,0 +1,22 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+import Card from './Card';
+
+import props from './mock';
+
+describe('<Card />', () => {
+  it('should render Card correctly', () => {
+    const { container } = render(<Card {...props} />);
+
+    const cardWrapper = container.querySelector('div');
+    const titleElement = container.querySelector('h3.sc-gtsrHT');
+    const amountElement = container.querySelector('span.sc-dlnjwi');
+    const percentageElement = container.querySelector('span.sc-hKFxyN');
+
+    expect(cardWrapper).toBeInTheDocument();
+    expect(titleElement).toBeInTheDocument();
+    expect(amountElement).toBeInTheDocument();
+    expect(percentageElement).toBeInTheDocument();
+  })
+})
