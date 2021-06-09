12a2239c284286d90838c248dd55cee93330a00f
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-09 17:29:41 -0400

    add fake user to login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./../../components/Button/Button";
-import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
 
 const BoxWrapper = styled.div`
   position: absolute;
@@ -19,18 +19,59 @@ const BoxWrapper = styled.div`
   padding: 15px 25px;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+const Input = styled.input`
+  height: 35px;
+  padding: 0 15px;
+  border: 1px solid gray;
+  border-radius: 5px;
+  &:focus {
+    outline: 2px solid purple;
+    border: none;
+  }
+`;
+
 export default function Login() {
   const history = useHistory();
-  
-  const handleLogin = () => {
-    history.push('/home')
+
+  function getFormData (event) {
+    const [username, password] = Array.from(
+      event.target.querySelectorAll("input")
+    ).map((item) => item.value);
+    
+    return [username, password]
   }
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const [ username, password ] = getFormData(event);
+    
+    const user = {
+      username: 'railson',
+      password: '12345678',
+    }
+
+    if (user.username === username && user.password === password) {
+      history.push('/home')
+    } else {
+      alert('Usuário ou senha inválida!')
+    }
+  };
 
   return (
     <>
       <BoxWrapper>
-        <label>Login</label>
-        <Button onClick={handleLogin} value="Entrar" />
+        <Form onSubmit={handleLogin}>
+          <label>Login</label>
+          <Input type="text" placeholder="Username" />
+          <Input type="password" placeholder="Senha" />
+          <Button value="Entrar" />
+        </Form>
       </BoxWrapper>
     </>
   );
