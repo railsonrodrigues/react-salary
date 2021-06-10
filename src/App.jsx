e968f7fdc341e3ead165ff7c650faeeddb47c5a1
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-10 10:59:14 -0400

    refactor: routes in file routes.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import HomePage from './pages/Home/Home.jsx';
-import AboutMePage from './pages/AboutMe/AboutMe.jsx';
-import LoginPage from './pages/Login/Login.jsx';
-import AccountPage from './pages/Account/Account.jsx';
-import PublicPage from './pages/PublicPage/PublicPage.jsx';
-import NotFound404Page from './pages/NotFound404/NotFound404.jsx';
+import { Routes } from './routes';
 
 export default function App () {
 
@@ -15,32 +10,6 @@ export default function App () {
   // }  // ONDE USAR useEffect -> didMount, didUnmout, e didUpdate
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact> {/*Public*/}
-          <PublicPage />
-        </Route>
-
-        <Route path="/login"> {/*Public*/}
-          <LoginPage />
-        </Route>
-
-        <Route path="/account">
-          <AccountPage />
-        </Route>
-
-        <Route path="/home">
-          <HomePage />
-        </Route>
-        
-        <Route path="/about-me">
-          <AboutMePage />
-        </Route>
-
-        <Route path="*"> {/*Public*/}
-          <NotFound404Page />
-        </Route>
-      </Switch>
-    </Router>
+    <Routes />
   )
 }
