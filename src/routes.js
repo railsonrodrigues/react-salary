7b99f7c482184581b1b1ede5e167448bc291c834
Railson Rodrigues <rrrailson1@gmail.com>
2021-06-10 11:53:53 -0400

    add fake authenticate

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import AboutMePage from './pages/AboutMe/AboutMe';
 import AccountPage from './pages/Account/Account';
 import LoginPage from './pages/Login/Login';
@@ -7,15 +7,26 @@ import PublicPage from './pages/PublicPage/PublicPage';
 import HomePage from './pages/Home/Home.jsx';
 import NotFound404Page from './pages/NotFound404/NotFound404';
 
+import { isAuthenticated } from './auth';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return <Route {...rest} render={props => 
+    isAuthenticated() ?
+      <Component />
+      :
+      <Redirect to={{ pathname: '/login', state: {from: props.location} }}/>
+  } />
+}
+
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={PublicPage} />
         <Route path='/login' component={LoginPage} />
-        <Route path='/account' component={AccountPage} />
-        <Route path='/home' component={HomePage} />
-        <Route path='/about-me' component={AboutMePage} />
+        <PrivateRoute path='/account' component={AccountPage} />
+        <PrivateRoute path='/home' component={HomePage} />
+        <PrivateRoute path='/about-me' component={AboutMePage} />
         <Route path='*' component={NotFound404Page} />
       </Switch>
     </Router>
